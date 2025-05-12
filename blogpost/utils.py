fe5ca63aa8736635189bc57f51d3b0b155917167
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-05-12 11:39:02 +0200

    complete blog post chapter "basics"

---
+++ blogpost/utils.py
@@ -0,0 +1,48 @@
+import torch
+import torchvision.transforms as T
+
+def encode_prompt(prompt, tokenizer, text_encoder):
+    # lower case prompt! took a long time to find that this is necessary: https://github.com/huggingface/diffusers/blob/e8aacda762e311505ba05ae340af23b149e37af3/src/diffusers/pipelines/sana/pipeline_sana.py#L433
+    tokenizer.padding_side = "right"
+    if isinstance(prompt, list):
+        prompt = [p.lower().strip() for p in prompt]
+    elif isinstance(prompt, str):
+        prompt = prompt.lower().strip()
+    else:
+        raise Exception(f"Unknown prompt type {type(prompt)}")         
+    prompt_tok = tokenizer(prompt, return_tensors="pt", return_attention_mask=True, padding="max_length", truncation=True, max_length=50, add_special_tokens=True).to(text_encoder.device)
+    with torch.no_grad():
+        prompt_encoded=text_encoder(**prompt_tok)
+    return prompt_encoded.last_hidden_state, prompt_tok.attention_mask
+
+def latent_to_PIL(latent, ae):
+    with torch.no_grad():
+        image_out = ae.decode(latent).sample.to("cpu")
+    
+    if image_out.size(0) == 1:
+        # Single image processing
+        image_out = torch.clamp_(image_out[0,:], -1, 1)
+        image_out = image_out * 0.5 + 0.5
+        return T.ToPILImage()(image_out.float())
+    else:
+        images = []
+        for img in image_out:
+            img = torch.clamp_(img, -1, 1)
+            img = img * 0.5 + 0.5
+            images.append(T.ToPILImage()(img.float()))
+        return images
+
+def PIL_to_latent(images, ae):
+    transform = T.Compose([
+        T.ToTensor(),
+        T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        lambda x: x.to(dtype=ae.dtype)
+    ])
+
+    if not isinstance(images, (list, tuple)): images = [images]
+    
+    images_tensors = torch.cat([transform(image)[None] for image in images])
+    
+    with torch.no_grad():
+        latent = ae.encode(images_tensors.to(ae.device))
+    return latent.latent
