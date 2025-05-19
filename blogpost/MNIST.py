a0865780ab778d2d56f389ea93f9d2cea8cdd57a
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-05-19 09:03:30 +0200

    MNIST done

---
+++ blogpost/MNIST.py
@@ -0,0 +1,159 @@
+import torch
+import torch.nn.functional as F
+import wandb
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import make_grid
+from transformers import set_seed, AutoModel, AutoTokenizer
+from diffusers import AutoencoderDC, SanaTransformer2DModel
+from datasets import load_dataset
+from functools import partial
+from utils import latent_to_PIL, PIL_to_latent, generate, encode_prompt, add_random_noise
+from tqdm import tqdm
+
+lr = 1e-3
+bs = 256
+epochs = 10
+
+te_repo = "answerdotai/ModernBERT-base"
+sana_repo = "Efficient-Large-Model/Sana_600M_1024px_diffusers"
+dataset_repo ="g-ronimo/MNIST_64px-latents_dc-ae-f32c32-sana-1.0"
+
+set_seed(42)
+
+# Choose device and dtype
+dtype = torch.bfloat16
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
+
+# Load the text encoder and AE
+tokenizer = AutoTokenizer.from_pretrained(te_repo, torch_dtype=dtype)
+text_encoder = AutoModel.from_pretrained(te_repo, torch_dtype=dtype).to(device)
+dcae = AutoencoderDC.from_pretrained(sana_repo, subfolder="vae", torch_dtype=dtype).to(device)
+
+# Initialize the DiT, DiT-S with 12 layers, hidden size 384
+config = SanaTransformer2DModel.load_config(sana_repo, subfolder="transformer")
+config["num_layers"] = 12
+config["caption_channels"] = 768
+config["num_attention_heads"] = 6
+config["attention_head_dim"] = 64
+config["cross_attention_dim"] = 384
+config["num_cross_attention_heads"] = 6
+
+transformer = SanaTransformer2DModel.from_config(config).to(dtype).to(device)
+
+transformer_params = sum(p.numel() for p in transformer.parameters() if p.requires_grad) / 1e6
+print(f"Transformer parameters: {transformer_params:.2f}M")
+
+# Fix generate with pipeline components
+generate = partial(
+    generate, 
+    transformer=transformer, 
+    tokenizer=tokenizer, 
+    text_encoder=text_encoder,
+    dcae=dcae
+)
+
+# Prepare dataloader
+def collate(items):
+    labels = [str(i["label"]) for i in items]
+    prompts_emb, prompts_atnmask = encode_prompt(labels, tokenizer, text_encoder, max_length=5)
+
+    latents = torch.cat(
+        [torch.Tensor(i["latent"]) for i in items]
+    ).to(dtype).to(device)
+
+    return labels, prompts_emb, prompts_atnmask, latents
+
+ds = load_dataset(dataset_repo)
+dataloader_train = DataLoader(ds["train"], batch_size=bs, collate_fn=collate)
+dataloader_eval = DataLoader(ds["test"], batch_size=256, collate_fn=collate)
+
+# Optimizer
+optimizer = torch.optim.AdamW(transformer.parameters(), lr=lr)
+
+# Eval: images
+def eval_images(num_rows=1, seed = None):
+    num_images = num_rows * 10
+    return transforms.ToPILImage()(
+        make_grid(
+            [
+                transforms.ToTensor()(
+                    generate(
+                        str(i % 10), 
+                        latent_dim = [1, 32, 2, 2],
+                        num_steps = 10,
+                        seed = seed
+                    )
+                ) 
+                for i in tqdm(range(num_images), "eval_images")
+            ], 
+            nrow=10
+        )
+    )
+
+# Eval: loss
+def eval_loss():
+    losses = []
+
+    for batch_num, (labels, prompts_emb, prompts_atnmask, latents) in tqdm(enumerate(dataloader_eval), "eval_loss"):
+        latents = latents * dcae.config["scaling_factor"]
+        latents_noisy, timestep, noise = add_random_noise(latents)
+        with torch.no_grad():
+            noise_pred = transformer(
+                hidden_states = latents_noisy.to(dtype), 
+                encoder_hidden_states = prompts_emb, 
+                encoder_attention_mask = prompts_atnmask,
+                timestep = timestep, 
+            ).sample
+    
+        loss = F.mse_loss(noise_pred, noise - latents)
+        losses.append(loss.item())  
+    return sum(losses)/len(losses)
+
+# Setup logging 
+wandb.init(
+    project="MNIST", 
+    name=f"{transformer_params:.1f}M_BS-{bs}_LR-{lr}_{epochs}-epochs"
+).log_code(".", include_fn=lambda path: path.endswith(".py") or path.endswith(".ipynb") or path.endswith(".json"))
+
+# TRAIN
+step = 0 
+for e in range(epochs):
+    for labels, prompts_emb, prompts_atnmask, latents in dataloader_train:
+        step += 1
+        epoch = step/len(dataloader_train)
+
+        # Scale latent and add random amount of noise
+        latents = latents * dcae.config["scaling_factor"]
+        latents_noisy, timestep, noise = add_random_noise(latents)
+        
+        # Get a noise prediction out of the model
+        noise_pred = transformer(
+            hidden_states = latents_noisy.to(dtype), 
+            encoder_hidden_states = prompts_emb, 
+            encoder_attention_mask = prompts_atnmask,
+            timestep = timestep, 
+        ).sample
+        
+        loss = F.mse_loss(noise_pred, noise - latents)
+        loss.backward()
+    
+        optimizer.step()
+        optimizer.zero_grad()
+    
+        if step % 10 == 0:
+            print(f"step {step} epoch {epoch:.2f} loss: {loss.item()}")
+            wandb.log({"step": step, "epoch": epoch, "loss_train": loss.item()})
+        if step % 100 == 0:
+            wandb.log({"epoch": round(epoch, 1), "step": step, "eval_images": wandb.Image(eval_images(seed=42))})
+
+    el = eval_loss()
+    print(f"eval step {step} epoch {epoch:.2f} loss: {el:.2f}")
+    wandb.log({"step": step, "epoch": epoch, "loss_eval": el})
+
+wandb.log({"final_gallery": wandb.Image(eval_images(num_rows=10))})
+
+wandb.finish()
+
+
+
--- blogpost/utils.py
+++ blogpost/utils.py
@@ -1,7 +1,62 @@
 import torch
 import torchvision.transforms as T
 
-def encode_prompt(prompt, tokenizer, text_encoder):
+def generate(prompt, transformer, tokenizer, text_encoder, dcae, num_steps = 10, latent_dim = [1, 32, 8, 8], seed=None):
+    # Encode the prompt
+    prompt_encoded, prompt_atnmask = encode_prompt(prompt, tokenizer, text_encoder)
+        
+    # Divide 1000 -> 0 in equally sized steps
+    timesteps = torch.linspace(1000, 0, num_steps + 1)
+    
+    # Noise level. 1.0 -> 0.0 in equally sized steps
+    sigmas = timesteps / 1000
+    
+    latent = torch.randn(latent_dim, generator=torch.manual_seed(seed) if seed else None).to(transformer.dtype).to(transformer.device)
+    
+    for t, sigma_prev, sigma_next, steps_left in zip(
+        timesteps, 
+        sigmas[:-1], 
+        sigmas[1:], 
+        range(num_steps, 0, -1)
+    ):
+        # DiT predicts noise
+        with torch.no_grad():
+            noise_pred = transformer(
+                latent, 
+                timestep=t[None].to(transformer.dtype).to(transformer.device), 
+                encoder_hidden_states=prompt_encoded, 
+                encoder_attention_mask=prompt_atnmask, 
+                return_dict=False
+            )[0]
+        # Remove noise from latent
+        latent = latent + (sigma_next - sigma_prev) * noise_pred 
+
+    return latent_to_PIL(latent / dcae.config["scaling_factor"], dcae)
+
+def add_random_noise(latents, timesteps=1000, dist="uniform"):
+    assert dist in ["normal", "uniform"], f"Requested sigma dist. {dist} not supported"
+
+    # batch size
+    bs = latents.size(0)
+
+    # gaussian noise
+    noise = torch.randn_like(latents)
+
+    # normal distributed sigmas
+    if dist == "normal":
+        sigmas = torch.randn((bs,)).sigmoid().to(latents.device)
+    else:
+        sigmas = torch.rand((bs,)).to(latents.device)
+    # sigmas = torch.randn((bs,)).sigmoid().to(latents.device)
+    
+    timesteps = (sigmas * timesteps).to(latents.device)   # yes, `timesteps = sigmas * 1000`, let's keep it simple
+    sigmas = sigmas.view([latents.size(0), *([1] * len(latents.shape[1:]))])
+    
+    latents_noisy = (1 - sigmas) * latents + sigmas * noise # (1-noise_level) * latent + noise_level * noise
+
+    return latents_noisy.to(latents.dtype), timesteps, noise
+    
+def encode_prompt(prompt, tokenizer, text_encoder, max_length=50):
     # lower case prompt! took a long time to find that this is necessary: https://github.com/huggingface/diffusers/blob/e8aacda762e311505ba05ae340af23b149e37af3/src/diffusers/pipelines/sana/pipeline_sana.py#L433
     tokenizer.padding_side = "right"
     if isinstance(prompt, list):
@@ -10,7 +65,7 @@ def encode_prompt(prompt, tokenizer, text_encoder):
         prompt = prompt.lower().strip()
     else:
         raise Exception(f"Unknown prompt type {type(prompt)}")         
-    prompt_tok = tokenizer(prompt, return_tensors="pt", return_attention_mask=True, padding="max_length", truncation=True, max_length=50, add_special_tokens=True).to(text_encoder.device)
+    prompt_tok = tokenizer(prompt, return_tensors="pt", return_attention_mask=True, padding="max_length", truncation=True, max_length=max_length, add_special_tokens=True).to(text_encoder.device)
     with torch.no_grad():
         prompt_encoded=text_encoder(**prompt_tok)
     return prompt_encoded.last_hidden_state, prompt_tok.attention_mask
