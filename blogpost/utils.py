db4a2f7957869674b33cb4b04ca5757c81248c0e
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-05-20 07:10:54 +0200

    add comments

--- blogpost/utils.py
+++ blogpost/utils.py
@@ -11,7 +11,11 @@ def generate(prompt, transformer, tokenizer, text_encoder, dcae, num_steps = 10,
     # Noise level. 1.0 -> 0.0 in equally sized steps
     sigmas = timesteps / 1000
     
-    latent = torch.randn(latent_dim, generator=torch.manual_seed(seed) if seed else None).to(transformer.dtype).to(transformer.device)
+    # Latent with gaussian noise
+    latent = torch.randn(
+        latent_dim, 
+        generator=torch.manual_seed(seed) if seed else None
+    ).to(transformer.dtype).to(transformer.device)
     
     for t, sigma_prev, sigma_next, steps_left in zip(
         timesteps, 
@@ -28,6 +32,7 @@ def generate(prompt, transformer, tokenizer, text_encoder, dcae, num_steps = 10,
                 encoder_attention_mask=prompt_atnmask, 
                 return_dict=False
             )[0]
+
         # Remove noise from latent
         latent = latent + (sigma_next - sigma_prev) * noise_pred 
 
@@ -42,40 +47,45 @@ def add_random_noise(latents, timesteps=1000, dist="uniform"):
     # gaussian noise
     noise = torch.randn_like(latents)
 
-    # normal distributed sigmas
+    # noise level distribution
     if dist == "normal":
+        # normal distributed sigmas
         sigmas = torch.randn((bs,)).sigmoid().to(latents.device)
     else:
+        # uniform
         sigmas = torch.rand((bs,)).to(latents.device)
-    # sigmas = torch.randn((bs,)).sigmoid().to(latents.device)
     
     timesteps = (sigmas * timesteps).to(latents.device)   # yes, `timesteps = sigmas * 1000`, let's keep it simple
     sigmas = sigmas.view([latents.size(0), *([1] * len(latents.shape[1:]))])
     
-    latents_noisy = (1 - sigmas) * latents + sigmas * noise # (1-noise_level) * latent + noise_level * noise
+    latents_noisy = (1 - sigmas) * latents + sigmas * noise 
 
     return latents_noisy.to(latents.dtype), timesteps, noise
     
 def encode_prompt(prompt, tokenizer, text_encoder, max_length=50):
-    # lower case prompt! took a long time to find that this is necessary: https://github.com/huggingface/diffusers/blob/e8aacda762e311505ba05ae340af23b149e37af3/src/diffusers/pipelines/sana/pipeline_sana.py#L433
     tokenizer.padding_side = "right"
+
+    # Sana wants lowercase prompts. https://github.com/huggingface/diffusers/blob/e8aacda762e311505ba05ae340af23b149e37af3/src/diffusers/pipelines/sana/pipeline_sana.py#L433
     if isinstance(prompt, list):
         prompt = [p.lower().strip() for p in prompt]
     elif isinstance(prompt, str):
         prompt = prompt.lower().strip()
     else:
-        raise Exception(f"Unknown prompt type {type(prompt)}")         
+        raise Exception(f"Unknown prompt type {type(prompt)}")   
+
     prompt_tok = tokenizer(prompt, return_tensors="pt", return_attention_mask=True, padding="max_length", truncation=True, max_length=max_length, add_special_tokens=True).to(text_encoder.device)
+
     with torch.no_grad():
         prompt_encoded=text_encoder(**prompt_tok)
+
     return prompt_encoded.last_hidden_state, prompt_tok.attention_mask
 
 def latent_to_PIL(latent, ae):
     with torch.no_grad():
         image_out = ae.decode(latent).sample.to("cpu")
-    
+
+    # If batch dimension is 1 return a single image, otherwise a list of images
     if image_out.size(0) == 1:
-        # Single image processing
         image_out = torch.clamp_(image_out[0,:], -1, 1)
         image_out = image_out * 0.5 + 0.5
         return T.ToPILImage()(image_out.float())
@@ -88,13 +98,15 @@ def latent_to_PIL(latent, ae):
         return images
 
 def PIL_to_latent(images, ae):
+    # Convert image to tensor and normalize
     transform = T.Compose([
         T.ToTensor(),
         T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         lambda x: x.to(dtype=ae.dtype)
     ])
 
-    if not isinstance(images, (list, tuple)): images = [images]
+    if not isinstance(images, (list, tuple)): 
+        images = [images]
     
     images_tensors = torch.cat([transform(image)[None] for image in images])
     
